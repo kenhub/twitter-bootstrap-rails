3aa697fbd9dd5cb85bb8a00423997152503275bd
JoKer <joker777vs.666@gmail.com>
2013-03-15 16:09:40 +0100

    fixes carousel bug that data-* attributes are not evaluated

--- vendor/assets/javascripts/twitter/bootstrap/bootstrap-carousel.js
+++ vendor/assets/javascripts/twitter/bootstrap/bootstrap-carousel.js
@@ -161,7 +161,7 @@
     return this.each(function () {
       var $this = $(this)
         , data = $this.data('carousel')
-        , options = $.extend({}, $.fn.carousel.defaults, typeof option == 'object' && option)
+        , options = $.extend({}, $.fn.carousel.defaults, typeof option == 'object' && option, $this.data())
         , action = typeof option == 'string' ? option : options.slide
       if (!data) $this.data('carousel', (data = new Carousel(this, options)))
       if (typeof option == 'number') data.to(option)
